12d6aead8287c286660bc4397cfc3c8bb418ba5a
Thirunavukkarasu <tamilan.arasu@gmail.com>
2023-04-02 00:00:51 +0530

    added multiple webhook endpoints

--- backend/controllers/webhooks.ts
+++ backend/controllers/webhooks.ts
@@ -1,9 +1,25 @@
 import { triggerQueue } from "../queues";
 
-export const webhookHandler = async (req: any, res: any) => {
+export const simpleWebhookHandler = async (req: any, res: any) => {
   const message = req.body.message || "User Onboarded";
   const userEmail = req.body.email || "tamilan.arasu@gmail.com";
-  await triggerQueue.add(`job`, { message, userEmail });
+  await triggerQueue.add(`simple-job`, { message, userEmail });
+
+  return res.json({ message: "Receieved" });
+};
+
+export const complexWebhookHandler = async (req: any, res: any) => {
+  const message = req.body.message || "User Onboarded";
+  const userEmail = req.body.email || "tamilan.arasu@gmail.com";
+  await triggerQueue.add(`complex-job`, { message, userEmail });
+
+  return res.json({ message: "Receieved" });
+};
+
+export const randomWebhookHandler = async (req: any, res: any) => {
+  const message = req.body.message || "User Onboarded";
+  const userEmail = req.body.email || "tamilan.arasu@gmail.com";
+  await triggerQueue.add(`random-job`, { message, userEmail });
 
   return res.json({ message: "Receieved" });
 };
--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,11 @@ import { ExpressAdapter } from "@bull-board/express";
 import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
 import { createBullBoard } from "@bull-board/api";
 import { triggerQueue } from "./queues";
-import { webhookHandler } from "./controllers/webhooks";
+import {
+  simpleWebhookHandler,
+  complexWebhookHandler,
+  randomWebhookHandler,
+} from "./controllers/webhooks";
 
 const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath("/admin/queues");
@@ -18,7 +22,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use("/admin/queues", serverAdapter.getRouter());
-app.post("/webhook", webhookHandler);
+app.post("/webhook/simple", simpleWebhookHandler);
+app.post("/webhook/complex", complexWebhookHandler);
+app.post("/webhook/random", randomWebhookHandler);
 
 app.listen(PORT, () => {
   console.log(`Started server in port ${PORT}`);
